0ed0a5aa7c8fc546fb770ceec63d816341977bf8
Dennis-ux-ai <dennissabu444@gmail.com>
2025-03-23 22:42:43 +0530

    c programing loops

---
+++ 1to100.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+    int i;
+    for(int i=0;i<=100;i++)
+    printf("%d",i);
+    
+
+    int n=0;
+    while(n<=100){
+        printf("%d",n);
+        n++;
+    }
+
+    int j=0;
+    do{
+       
+        printf("%d",j);
+        j++;
+    }
+    while (j<=100);
+
+}
