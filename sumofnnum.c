5e5a2b7b20b0306fbd596787b02cc505b0c2fefb
Dennis-ux-ai <dennissabu444@gmail.com>
2025-03-27 21:25:24 +0530

    Refactor sum of numbers program to include average calculation and user input for multiple numbers

--- sumofnnum.c
+++ sumofnnum.c
@@ -1,13 +1,26 @@
-#include<stdio.h>
-void main(){
-    int n,i,sum=0;
-    printf("Enter an number\n");
-    scanf("%d",&n);
-    for(i=0;i<=n;i++){
-        sum=sum+i;
-    
 
-    }
-printf("sum of %d numbers is%d \n",n,sum);
+#include <stdio.h>
 
+void main() {
+  int i, n, sum = 0, avg, arr[100];
+  
+  // Prompt the user for input
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  
+  // Prompt the user to enter the numbers
+  printf("Enter the %d numbers:\n", n);
+  
+  // Loop to read the numbers and calculate the sum
+  for (i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+    sum = sum + arr[i];
+  }
+  
+  // Calculate the average
+  avg = sum / n;
+  
+  // Print the sum and average
+  printf("Sum of the %d numbers is %d\n", n, sum);
+  printf("Average of the numbers is %d\n", avg);
 }
